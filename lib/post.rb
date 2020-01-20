39f0585efce01f2ae1998b5dccc520860f34f893
Tanya Bihag <tbihag@gmail.com>
2020-01-20 23:51:26 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -6,7 +6,12 @@ def initialize(title)
   @title = title 
 end 
 
-
-
+book_title = Post.new("Book")
+moby_dick = Author.new("Moby Dick")
+book_title.author = moby_dick
 end
 
+drake = Artist.new("Drake", "rap")
+hotline_bling = Song.new("Hotline Bling")
+ 
+hotline_bling.artist = drake
