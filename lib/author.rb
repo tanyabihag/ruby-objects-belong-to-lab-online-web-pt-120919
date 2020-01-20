33424915948ca044985b94fa2833040ea31a13a0
Tanya Bihag <tbihag@gmail.com>
2020-01-20 23:58:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,11 +2,8 @@ class Author
 
 attr_accessor :name
 
-def initialize(name)
+def initialize
   @name = name 
 end
 end
 
-tanya = Author.new("Tanya")
-moby_dick = Post.new("Moby Dick")
-moby_dick.author = tanya
